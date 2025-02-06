ac4c3da35a23a79dc103faf8f52dfde2279cd528
pio <pio@noncelab.com>
2025-02-06 10:16:28 +0900

    feat: define CoconutLayout and apply to example

---
+++ lib/src/common/sizes.dart
@@ -0,0 +1,27 @@
+class Sizes {
+  static const size2 = 2.0;
+  static const size4 = 4.0;
+  static const size6 = 6.0;
+  static const size8 = 8.0;
+  static const size10 = 10.0;
+  static const size12 = 12.0;
+  static const size14 = 14.0;
+  static const size16 = 16.0;
+  static const size18 = 18.0;
+  static const size20 = 20.0;
+  static const size22 = 22.0;
+  static const size24 = 24.0;
+  static const size26 = 26.0;
+  static const size28 = 28.0;
+  static const size30 = 30.0;
+  static const size32 = 32.0;
+  static const size34 = 34.0;
+  static const size36 = 36.0;
+  static const size38 = 38.0;
+  static const size40 = 40.0;
+  static const size44 = 44.0;
+  static const size48 = 48.0;
+  static const size52 = 52.0;
+  static const size56 = 56.0;
+  static const size60 = 60.0;
+}
