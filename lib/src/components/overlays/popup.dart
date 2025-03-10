a1c7c596cc372c3f82f6c98e0e51afd7d9935e55
pio <pio@noncelab.com>
2025-03-10 14:49:09 +0900

    feat: Add ClipRRect and update pressing color in CoconutPopup

--- lib/src/components/overlays/popup.dart
+++ lib/src/components/overlays/popup.dart
@@ -99,122 +99,126 @@ class _CoconutPopupState extends State<CoconutPopup> {
     Brightness brightness = Theme.of(context).brightness;
 
     return Dialog(
-      child: Container(
-        decoration: BoxDecoration(
-          color: widget.backgroundColor ?? CoconutColors.onWhite(brightness),
-          borderRadius: const BorderRadius.all(Radius.circular(16)),
-        ),
-        child: IntrinsicHeight(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              /// Title Section
-              Container(
-                padding: widget.titlePadding ??
-                    const EdgeInsets.only(top: 24, bottom: 12),
-                child: Text(
-                  widget.title,
-                  style: CoconutTypography.body1_16_Bold.setColor(
-                    widget.titleColor ?? CoconutColors.onGray900(brightness),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(Radius.circular(16)),
+        child: Container(
+          decoration: BoxDecoration(
+            color: widget.backgroundColor ?? CoconutColors.onWhite(brightness),
+            borderRadius: const BorderRadius.all(Radius.circular(16)),
+          ),
+          child: IntrinsicHeight(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                /// Title Section
+                Container(
+                  padding: widget.titlePadding ??
+                      const EdgeInsets.only(top: 24, bottom: 12),
+                  child: Text(
+                    widget.title,
+                    style: CoconutTypography.body1_16_Bold.setColor(
+                      widget.titleColor ?? CoconutColors.onGray900(brightness),
+                    ),
                   ),
                 ),
-              ),
 
-              /// Description Section
-              Container(
-                alignment: Alignment.topCenter,
-                padding: widget.descriptionPadding ??
-                    const EdgeInsets.only(
-                        left: 24, right: 24, top: 12, bottom: 12),
-                constraints: const BoxConstraints(minHeight: 66),
-                child: Text(
-                  widget.description,
-                  textAlign: widget.centerDescription ? TextAlign.center : null,
-                  style: CoconutTypography.body1_16.setColor(
-                    widget.descriptionColor ??
-                        CoconutColors.onGray800(brightness),
+                /// Description Section
+                Container(
+                  alignment: Alignment.topCenter,
+                  padding: widget.descriptionPadding ??
+                      const EdgeInsets.only(
+                          left: 24, right: 24, top: 12, bottom: 12),
+                  constraints: const BoxConstraints(minHeight: 66),
+                  child: Text(
+                    widget.description,
+                    textAlign:
+                        widget.centerDescription ? TextAlign.center : null,
+                    style: CoconutTypography.body1_16.setColor(
+                      widget.descriptionColor ??
+                          CoconutColors.onGray800(brightness),
+                    ),
                   ),
                 ),
-              ),
 
-              /// Action Buttons (Left & Right)
-              Row(
-                children: [
-                  if (widget.onTapLeft != null) ...{
+                /// Action Buttons (Left & Right)
+                Row(
+                  children: [
+                    if (widget.onTapLeft != null) ...{
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _isLeftButtonPressing = false;
+                            });
+                            widget.onTapLeft?.call();
+                          },
+                          onTapCancel: () {
+                            setState(() {
+                              _isLeftButtonPressing = false;
+                            });
+                          },
+                          onTapDown: (details) {
+                            setState(() {
+                              _isLeftButtonPressing = true;
+                            });
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.only(top: 16, bottom: 16),
+                            color: _isLeftButtonPressing
+                                ? CoconutColors.surface(brightness)
+                                    .withOpacity(0.5)
+                                : Colors.transparent,
+                            alignment: Alignment.center,
+                            child: Text(
+                              widget.leftButtonText,
+                              style: CoconutTypography.body1_16.setColor(
+                                widget.leftButtonColor ??
+                                    CoconutColors.onGray900(brightness),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    },
                     Expanded(
                       child: GestureDetector(
                         onTap: () {
                           setState(() {
-                            _isLeftButtonPressing = false;
+                            _isRightButtonPressing = false;
                           });
-                          widget.onTapLeft?.call();
+                          widget.onTapRight.call();
                         },
                         onTapCancel: () {
                           setState(() {
-                            _isLeftButtonPressing = false;
+                            _isRightButtonPressing = false;
                           });
                         },
                         onTapDown: (details) {
                           setState(() {
-                            _isLeftButtonPressing = true;
+                            _isRightButtonPressing = true;
                           });
                         },
                         child: Container(
                           padding: const EdgeInsets.only(top: 16, bottom: 16),
-                          color: _isLeftButtonPressing
+                          color: _isRightButtonPressing
                               ? CoconutColors.onPrimary(brightness)
                                   .withOpacity(0.5)
                               : Colors.transparent,
                           alignment: Alignment.center,
                           child: Text(
-                            widget.leftButtonText,
-                            style: CoconutTypography.body1_16.setColor(
-                              widget.leftButtonColor ??
+                            widget.rightButtonText,
+                            style: CoconutTypography.body1_16_Bold.setColor(
+                              widget.rightButtonColor ??
                                   CoconutColors.onGray900(brightness),
                             ),
                           ),
                         ),
                       ),
                     ),
-                  },
-                  Expanded(
-                    child: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _isRightButtonPressing = false;
-                        });
-                        widget.onTapRight.call();
-                      },
-                      onTapCancel: () {
-                        setState(() {
-                          _isRightButtonPressing = false;
-                        });
-                      },
-                      onTapDown: (details) {
-                        setState(() {
-                          _isRightButtonPressing = true;
-                        });
-                      },
-                      child: Container(
-                        padding: const EdgeInsets.only(top: 16, bottom: 16),
-                        color: _isRightButtonPressing
-                            ? CoconutColors.onPrimary(brightness)
-                                .withOpacity(0.5)
-                            : Colors.transparent,
-                        alignment: Alignment.center,
-                        child: Text(
-                          widget.rightButtonText,
-                          style: CoconutTypography.body1_16_Bold.setColor(
-                            widget.rightButtonColor ??
-                                CoconutColors.onGray900(brightness),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              )
-            ],
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
