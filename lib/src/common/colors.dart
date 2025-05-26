42cb7467e2132a3b336b2cf9657673d47fe1113e
pio <pio@noncelab.com>
2025-05-26 14:34:19 +0900

    feat(color): add searchbarBackground, searchbarHint and searchbarText Colors in `CoconutColors`

--- lib/src/common/colors.dart
+++ lib/src/common/colors.dart
@@ -21,6 +21,9 @@ class CoconutColors {
   static const Color gray900 = Color(0xFF1C1C1C);
   static const Color borderGray = Color.fromRGBO(81, 81, 96, 1);
   static const Color borderLightGray = Color.fromRGBO(235, 231, 228, 1);
+  static const Color searchbarBackground = Color.fromRGBO(241, 242, 245, 1);
+  static const Color searchbarHint = Color.fromRGBO(154, 158, 168, 1);
+  static const Color searchbarText = Color.fromRGBO(20, 23, 24, 1);
 
   static const Color hotPink = Color(0xFFDA415C);
   static const Color hotPink150 = Color(0x26DA415C);
