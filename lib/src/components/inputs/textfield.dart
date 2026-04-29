062d6185a978e42b93294688736585e001023dff
roy-noncelab <roy@noncelab.com>
2026-04-29 19:02:42 +0900

    fix(textfield): modified constraints

--- lib/src/components/inputs/textfield.dart
+++ lib/src/components/inputs/textfield.dart
@@ -330,7 +330,8 @@ class _CoconutTextFieldState extends State<CoconutTextField> {
   void _updateData() {
     _activeColor = widget.activeColor ?? CoconutColors.onBlack(brightness);
     _cursorColor = widget.cursorColor ?? CoconutColors.onBlack(brightness);
-    _placeholderColor = widget.placeholderColor ?? CoconutColors.onGray300(brightness);
+    _placeholderColor =
+        widget.placeholderColor ?? CoconutColors.onGray300(brightness);
     _errorColor = widget.errorColor ?? CoconutColors.red;
     _backgroundColor = widget.backgroundColor ?? Colors.transparent;
     _text = widget.controller.text;
@@ -365,12 +366,14 @@ class _CoconutTextFieldState extends State<CoconutTextField> {
     Size nextSuffixSize = const Size(0, 0);
 
     if (_prefixGlobalKey.currentContext != null) {
-      final prefixRenderBox = _prefixGlobalKey.currentContext?.findRenderObject() as RenderBox;
+      final prefixRenderBox =
+          _prefixGlobalKey.currentContext?.findRenderObject() as RenderBox;
       nextPrefixSize = prefixRenderBox.size;
     }
 
     if (_suffixGlobalKey.currentContext != null) {
-      final suffixRenderBox = _suffixGlobalKey.currentContext?.findRenderObject() as RenderBox;
+      final suffixRenderBox =
+          _suffixGlobalKey.currentContext?.findRenderObject() as RenderBox;
       nextSuffixSize = suffixRenderBox.size;
     }
 
@@ -419,7 +422,8 @@ class _CoconutTextFieldState extends State<CoconutTextField> {
                         color: resolvedBorderColor,
                       )
                 : null,
-            borderRadius: isUnderline ? null : BorderRadius.circular(widget.borderRadius),
+            borderRadius:
+                isUnderline ? null : BorderRadius.circular(widget.borderRadius),
             color: _backgroundColor,
           ),
           child: Stack(
@@ -458,21 +462,26 @@ class _CoconutTextFieldState extends State<CoconutTextField> {
                 autocorrect: widget.autocorrect,
                 enableSuggestions: widget.enableSuggestions,
                 onEditingComplete: widget.onEditingComplete,
-                onTapOutside: widget.unfocusOnTapOutside ? (_) => widget.focusNode.unfocus() : null,
+                onTapOutside: widget.unfocusOnTapOutside
+                    ? (_) => widget.focusNode.unfocus()
+                    : null,
                 enabled: widget.enabled,
               ),
               IgnorePointer(
                 child: Container(
-                  height: widget.prefix != null ? _prefixSize.height : null,
-                  margin: widget.prefix == null
-                      ? resolvedPadding
-                      : EdgeInsets.only(
-                          left: _prefixSize.width,
-                          top: resolvedPadding.top,
-                        ),
-                  padding: widget.suffix != null ? EdgeInsets.only(right: _suffixSize.width) : null,
+                  margin: EdgeInsets.only(
+                    left: widget.prefix == null
+                        ? resolvedPadding.left
+                        : _prefixSize.width,
+                    top: resolvedPadding.top,
+                    right: widget.suffix != null
+                        ? _suffixSize.width + resolvedPadding.right
+                        : resolvedPadding.right,
+                  ),
                   alignment: Alignment.centerLeft,
-                  child: widget.placeholderText == null || _isFocus || _text.isNotEmpty
+                  child: widget.placeholderText == null ||
+                          _isFocus ||
+                          _text.isNotEmpty
                       ? Text(
                           '',
                           style: CoconutTypography.body2_14.copyWith(
@@ -499,8 +508,7 @@ class _CoconutTextFieldState extends State<CoconutTextField> {
                 Positioned.fill(
                   child: Padding(
                     padding: EdgeInsets.only(
-                      top: resolvedPadding.top,
-                      bottom: resolvedPadding.bottom,
+                      right: resolvedPadding.right,
                     ),
                     child: Align(
                       alignment: Alignment.centerRight,
